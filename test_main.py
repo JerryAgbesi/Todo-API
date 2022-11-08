ffb0ec617b566ffb6c77cc037d785c30f5edbe4e
JerryAgbesi <jerryeagbesi@gmail.com>
2022-11-08 14:56:23 +0000

    Write test for single todo item

--- test_main.py
+++ test_main.py
@@ -6,7 +6,12 @@ test_client = TestClient(app)
 
 
 
-def test_get_todos():
-    response = test_client.get('/todo')
+def test_get_todo():
+    response = test_client.get('/todo/2')
     assert response.status_code == 200
-    # assert response.json() == {"message":"root read successful"}
+    assert response.json() == {
+    "id": 2,
+    "task": "read a book",
+    "description": "Finish a book for the month",
+    "date_created": "2022-11-08T13:44:47.900453"
+}
